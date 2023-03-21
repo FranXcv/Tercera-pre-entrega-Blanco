d5b7f69010961ddcc4ae6d5d6b47b799e6eabca9
Franco <blancofranco205@gmail.com>
2023-03-20 22:56:23 -0300

    primer commit

--- preentrega/urls.py
+++ preentrega/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("profesores/", profesores, name="profe"),
     path("cursos/", cursos, name="cursos"),
     path("entregables/", entregables, name="entregables"),
+    path("CursoFormulario/", cursoFormulario, name="Agrega_curso"),
+    path("EntregableFormulario/", entregableFormulario, name="Agrega_entregable"),
+    path("EstudianteFormulario/", estudianteFormulario, name="Agrega_estudiante"),
+    path("ProfesorFormulario/", profesorFormulario, name="Agrega_profesor"),
 ]
